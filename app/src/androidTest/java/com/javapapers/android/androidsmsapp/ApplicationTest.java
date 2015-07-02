ef8457a1101e1571dda86d784182ad7dd06f765f
Ricatdo Luis Baumann <ricardo.luis.baumann@gmail.com>
2015-07-02 18:33:57 -0300

    sms test

--- app/src/androidTest/java/com/javapapers/android/androidsmsapp/ApplicationTest.java
+++ app/src/androidTest/java/com/javapapers/android/androidsmsapp/ApplicationTest.java
@@ -3,10 +3,15 @@ package com.javapapers.android.androidsmsapp;
 import android.app.Application;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.hamcrest.CoreMatchers;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -14,6 +19,13 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.hamcrest.core.Is.is;
 
 @LargeTest
 public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -38,16 +50,20 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
         onView(withId(R.id.editTextPhoneNo)).perform(typeText("5184117032"));
         onView(withId(R.id.editTextSMS)).perform(typeText("mensagem ui loco"));
 
-        onView(withId(R.id.btnSendSMS)).perform(click());
+        //onView(withId(R.id.btnSendSMS)).perform(click());
 
-        Thread.sleep(500000);
+        //Thread.sleep(50000);
 
         onView(withId(R.id.btnInbox)).check(matches(isDisplayed()));
         onView(withId(R.id.btnInbox)).perform(click());
 
+        onData(allOf(is(instanceOf(String.class)), containsString("Dieine")))
+                .perform(click());
+
+        //onData(allOf(is(instanceOf(String.class)), containsString("estegosauro")))
+               // .perform(click());
 
 
-        onView(withId(R.id.textView)).check(matches(withText("dieine")));
     }
 
 }
